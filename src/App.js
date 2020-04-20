66b802e2129fad9caea573c157bb7e187590d22a
Yehor Melnyk <egorchikmelnyk@gmail.com>
2020-04-19 22:19:52 -0700

    fixed history routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
-import { Router, Switch, Route } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
+import { Switch, Route } from 'react-router-dom';
 import styles from './App.module.scss';
 import routes from './routes';
-
-const history = createBrowserHistory();
 
 export const App = () => {
   return (
     <div className={styles.bodyContainer}>
-      <Router history={history}>
-        <div>
-          <Switch>
-            {routes.map(route => (
-              <Route {...route} />
-            ))}
-          </Switch>
-        </div>
-      </Router>
+      <div>
+        <Switch>
+          {routes.map(route => (
+            <Route {...route} />
+          ))}
+        </Switch>
+      </div>
     </div>
   );
 }
